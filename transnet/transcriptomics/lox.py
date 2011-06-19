fea1ab9edc2a882dc1b05d134fd6b4bd846876b8
Matt Peterson <matt@matthew-peterson.net>
2011-06-19 14:04:24 -0400

    LOX I/O scaffolding

---
+++ transnet/transcriptomics/lox.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Provides classes for working with the output from LOX
+
+Zhang, Z., F. Lopez-Giraldez and J.P. Townsend, 2010. 
+LOX: inferring Level Of eXpression from diverse methods of census sequencing. 
+Bioinformatics 26: 1918-1919.
+"""
+
+def read(lox_handle, read_pvals = False):
+    pass
+
+class LOXExperiment(object):
+    """
+    Describes a LOX experiment
+    """
+    def __init__(lox_handle):
+        pass
+
+    def _parse_first_line(handle):
+        pass
+    
