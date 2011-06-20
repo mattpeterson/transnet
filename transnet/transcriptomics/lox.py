c644e0c4c66faabff373fc4c0755cccf8d90c2f1
Matthew Peterson <mpeterson@mitre.org>
2011-06-20 13:40:42 -0400

    __init__ for transcriptomics module

--- transnet/transcriptomics/lox.py
+++ transnet/transcriptomics/lox.py
@@ -8,7 +8,20 @@ Bioinformatics 26: 1918-1919.
 """
 
 def read(lox_handle, read_pvals = False):
-    pass
+    """Reads in a LOX output.
+    
+    :param lox_handle: A handle to the LOX output
+    :type lox_handle: file
+    :param read_pvals: Read the p-values for differential expression from the
+                       directory the LOX output is in.  If True, will look for
+                       the files in the directory, and read them into the
+                       pvals attribute
+    """
+    experiment = LOXExperiment(lox_handle)
+    
+    # TODO: insert pval logic here
+    
+    return experiment
 
 class LOXMeasurement(object):
     
@@ -30,11 +43,33 @@ class LOXMeasurement(object):
 
 class LOXExperiment(object):
     """
-    Describes a LOX experiment
+    Describes a LOX experiment.  That is, the results from a run of LOX.  This
+    is dependent on the input set.
     """
     def __init__(self, lox_handle):
         self.measurements = []
         self.experiments = []
+        self._parse_first_line(lox_handle)
+        self._parse_lox_output(lox_handle)
+    
+    def _parse_lox_output(self, handle):
+        """
+        Reads the rest of the values from the LOX file and loads in the
+        measurement data
+        
+        param: handle: The file handle to read from
+        type: handle: file 
+        """
+        for line in handle:
+            tokens = line.rstrip("\r\n").split("\t")
+            # Create a new measurement for each 
+            num_experiments = len(self.experiments)
+            for i in range(0, num_experiments):
+                value = float(tokens[2 + i])
+                lower_bound = float(tokens[2 + i + num_experiments])
+                upper_bound = float(tokens[2 + i + 2*num_experiments])
+                measurement = LOXMeasurement(value, lower_bound, upper_bound)
+                self.measurements[i][tokens[0]] = measurement
 
     def _parse_first_line(self, handle):
         """
@@ -46,7 +81,7 @@ class LOXExperiment(object):
         
         for i in range(2, 2 + num_experiments):
             self.measurements.append({})
-            self.indices.append(tokens[i])
+            self.experiments.append(tokens[i])
     
     def __getitem__(self, key):
         """
